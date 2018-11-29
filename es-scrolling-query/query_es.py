677ef7dd26b5b12119becc92c6a064dcfb0df8bd
Flavio Teixeira <flavio.silva@yandeh.com.br>
2018-11-29 15:32:24 -0200

    Added git ignore and refactore scrolling es query

--- es-scrolling-query/query_es.py
+++ es-scrolling-query/query_es.py
@@ -16,42 +16,38 @@ payload = {
     },
     "size": 1000
 }
+scroll_payload = {
+    'scroll': '2m'
+}
+
+# Response parser
+def parse_response(r_json):
+    # Returns empty if does not contain key '_scroll_id'
+    scroll_id = r_json.get('_scroll_id', '')
+    # Returns empty dict if does not contain key 'hits'
+    # Eventually, if this happens, the next get method will return an empty list
+    records = r_json.get('hits', {}).get('hits', [])
+    return records, scroll_id
 
 #Query first page
 first_page_response = requests.post(ES_FIRST_SCROLL_URI, json=payload)
 
-# Verify first response
-try:
-    r_json = first_page_response.json()
-    scroll_id = r_json['_scroll_id']
-    records = r_json['hits']['hits']
-except KeyError:
-    records = []
-    scroll_id = None
-    print(f'Error! Response: {first_page_response.json()}')
+# Parse first response
+records, scroll_id = parse_response(first_page_response.json())
 
 # While there are results, do logic and continue scrolling
 while len(records) > 0:
     count += len(records)
     print(f'Parsing {len(records)} records...')
-    ## DO LOGIC
+    ## DO
+    ## LOGIC
+    ## HERE
     print(f'Total until now: {count}')
     
-    # Continue scrolling
-    scroll_payload = {
-        'scroll': '2m',
-        'scroll_id': scroll_id
-    }
-    scroll_response = requests.post(ES_SCROLL_URI, json=scroll_payload)
-
-    try:
-        r_json = scroll_response.json()
-        scroll_id = r_json['_scroll_id']
-        records = r_json['hits']['hits']
-    except KeyError:
-        records = []
-        scroll_id = None
-        print(f'Error! Response: {first_page_response.json()}')
-
+    # Continue scrolling if there is new page
+    if scroll_id != '':
+        scroll_payload['scroll_id'] = scroll_id
+        scroll_response = requests.post(ES_SCROLL_URI, json=scroll_payload)
+        records, scroll_id = parse_response(scroll_response.json())
     
 print(f'Finished search with {len(file_list)} records found!')
