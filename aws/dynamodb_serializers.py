40d7a95d7eb339151fba9797a876eb25d794d503
Jose RF Junior <488409+GCPBigData@users.noreply.github.com>
2022-08-17 19:25:18 -0500

    Update dynamodb_serializers.py
    VERSAO LAMBDA, porem tem o BUG : https://github.com/boto/boto3/issues/369

--- aws/dynamodb_serializers.py
+++ aws/dynamodb_serializers.py
@@ -1,57 +1,47 @@
+import json
+import boto3
+import base64
 from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 
+output = []
+
+def lambda_handler(event, context):
+    
+    
+    for record in event['records']:
+        payload = base64.b64decode(record['data']).decode('utf-8')
+        
+
+        row_w_newline = payload + "\n"
+        
+        print('row_w_newline type:', type(row_w_newline))
+        x = parse_low_level_event(json.loads(row_w_newline))
+
+        row_w_newline = base64.b64encode(str(x).encode('utf-8'))
+
+        output_record = {
+            'recordId': record['recordId'],
+            'result': 'Ok',
+            'data': row_w_newline
+        }
+        output.append(output_record)
+
+    print('Processed {} records.'.format(len(event['records'])))
+
+    return {'records': output}
 
 def dynamo_obj_to_python_obj(dynamo_obj: dict) -> dict:
     deserializer = TypeDeserializer()
     return {
-        k: deserializer.deserialize(v) 
+        k: deserializer.deserialize(v)
         for k, v in dynamo_obj.items()
-    }  
-
-
-def python_obj_to_dynamo_obj(python_obj: dict) -> dict:
-    serializer = TypeSerializer()
-    return {
-        k: serializer.serialize(v)
-        for k, v in python_obj.items()
     }
 
-
-def main():
-    p = {
-        'a': 1,
-        'b': 'b',
-        'c': {
-            'd': False
-        },
-        'e': [10, 20, 30]
-    }
-    print('Python object: ')
-    print(p)
-    print('Converted to DynamoDB object: ')
-    print(python_obj_to_dynamo_obj(p))
-
-    d = {
-        'a': {'N': '1'}, 
-        'b': {'S': 'b'}, 
-        'c': {
-            'M': {
-                'd': {'BOOL': False}
-            }
-        }, 
-        'e': {
-            'L': [
-                {'N': '10'}, 
-                {'N': '20'}, 
-                {'N': '30'}
-            ]
-        }
-    }
-    print('DynamoDB object: ')
-    print(d)
-    print('Converted to Python object: ')
-    print(dynamo_obj_to_python_obj(d))
-
-
-if __name__ == '__main__':
-    main()
+def parse_low_level_event(event):
+    print(type(event), " --> ", event)
+    raw_records = []
+    if event["eventName"] in ["MODIFY", "INSERT"]:
+        raw_records = dynamo_obj_to_python_obj(event["dynamodb"]["NewImage"])
+		
+		
+    return raw_records
